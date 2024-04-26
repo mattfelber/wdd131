189dc290de7863f011439d26c20e4d0b88881ddb
Matheus Felber <math.felber@gmail.com>
2024-04-26 14:22:23 -0300

    Last Modified
    added this to the footer

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+// Select the DOM element for output
+const full = document.querySelector("#full");
+
+// Use the last modified date of the document
+const lastModifiedDate = new Date(document.lastModified);
+
+// Display the last modified date in the "full" paragraph
+full.textContent = `Last modified: ${lastModifiedDate.toLocaleString()}`;
