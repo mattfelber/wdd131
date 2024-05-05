f12f9765f776a7769de58f9327c7b5b3e3751c36
Matheus Felber <math.felber@gmail.com>
2024-05-05 02:44:44 -0300

    hamburger functionality

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -6,3 +6,11 @@ const lastModifiedDate = new Date(document.lastModified);
 
 // Display the last modified date in the "full" paragraph
 outputElement.textContent = `©️ 2024 | Last modified: ${lastModifiedDate.toLocaleString()}`;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
