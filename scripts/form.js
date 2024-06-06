528a80aa940b69bbd511f564f3b7777079bf7f98
Matheus Felber <math.felber@gmail.com>
2024-06-05 22:46:55 -0300

    Form enhancement

--- scripts/form.js
+++ scripts/form.js
@@ -1,39 +1,49 @@
-const productSelect = document.getElementById('product');
-const products = [
-  {
-    id: "fc-1888",
-    name: "flux capacitor",
-    avgRating: 4.5 // Corrected typo in key name
-  },
-  {
-    id: "fc-2050",
-    name: "power laces",
-    averageRating: 4.7 // Corrected typo in key name
-  },
-  {
-    id: "fs-1987",
-    name: "time circuits",
-    averageRating: 3.5 // Corrected typo in key name
-  },
-  {
-    id: "ac-2000",
-    name: "low voltage reactor",
-    averageRating: 3.9 // Corrected typo in key name
-  },
-  {
-    id: "jj-1969",
-    name: "warp equalizer",
-    averageRating: 5.0 // Corrected typo in key name
-  }
-];
+const today = new Date();
+const year = document.querySelector("#currentyear");
+year.innerHTML = today.getFullYear();
 
-function populateProducts() {
-  products.forEach(product => {
-    const option = document.createElement('option');
-    option.value = product.id;
-    option.text = product.name;
-    productSelect.appendChild(option);
-  });
-}
+let modif = new Date(document.lastModified).toLocaleString('en-us');
+const lastModified = document.querySelector("#lastModified");
+lastModified.innerHTML = `Last Modified: <span>${modif}</span>`;
 
-populateProducts();
+// ProductS
+document.addEventListener('DOMContentLoaded', function () {
+    const products = [
+        { id: 'fc-1888', 
+        name: 'flux capacitor', 
+        avg_rating: 4.5 },
+
+        { id: 'fc-2050', 
+        name: 'power laces', 
+        avg_rating: 4.7 },
+
+        { id: 'fs-1987', 
+        name: 'time circuits', 
+        avg_rating: 3.5 },
+
+        { id: 'ac-2000', name: 
+        'low voltage reactor', 
+        avg_rating: 3.9 },
+
+        { id: 'jj-1969', name: 
+        'warp equalizer', 
+        avg_rating: 5.0 }
+    ];
+
+    const productName = document.getElementById('naming-product');
+    products.forEach(product => {
+        const option = document.createElement('option');
+        option.value = product.name;
+        option.textContent = product.name;
+        productName.appendChild(option);
+    });
+
+});
+
+function amountReview() {
+    const numReviews = document.querySelector(".reviewCounter");
+    let reviewCounter = localStorage.getItem('.reviewCounter') || 0;
+    reviewCounter++;
+    localStorage.getItem('reviewCounter', reviewCounter);
+    numReviews.textContent = reviewCounter;
+};
